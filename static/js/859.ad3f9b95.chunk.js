a60cf524a423623c2ead1a1467e3223127602c19
ViktorTyt <92685344+ViktorTyt@users.noreply.github.com>
2022-09-01 20:14:41 +0000

    Deploying to gh-pages from @ ViktorTyt/goit-react-hw-08-phonebook@8683f615e208075cf1b8f200c0ed2054e16d4031 🚀

--- static/js/859.ad3f9b95.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[859],{4859:function(e,o,n){n.r(o),n.d(o,{default:function(){return h}});var t=n(184),h=function(){return(0,t.jsx)("h1",{children:"Welcome to the PhoneBook"})}}}]);
-//# sourceMappingURL=859.ad3f9b95.chunk.js.map
---
+++ static/js/859.cddea965.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[859],{4859:function(e,o,n){n.r(o),n.d(o,{default:function(){return h}});var t=n(184),h=function(){return(0,t.jsx)("h2",{children:"Welcome to the PhoneBook"})}}}]);
+//# sourceMappingURL=859.cddea965.chunk.js.map
